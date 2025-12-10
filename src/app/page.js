f638d2c6e9d76ca61bf34ef4099d2cedb3da58f8
mohamadnifras <mohamadnifras2004@gmail.com>
2025-12-11 03:16:19 +0530

    feat(about): added welcome-page and photo-gallery - nifras

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import Vision from '../modules/abouts/contaner/Vision'
-import ContactSection from '../modules/contact/containers/GetInTouch'
-import ContactLeft from '../modules/contact/containers/ContactLeft'
+import WelcomeHome from '../modules/about/container/WelcomeHome'
+import HeadLine from '../shared/components/HeadLine'
+import PhotoGallery from '../modules/about/container/PhotoGallery'
+// import Vision from '../modules/abouts/contaner/Vision'
+// import ContactSection from '../modules/contact/containers/GetInTouch'
+// import ContactLeft from '../modules/contact/containers/ContactLeft'
 // import OutlineButton from '../shared/components/OutlineButton'
-import Service from '../modules/service/container/Service'
-import Gallery from '../modules/gallery/containers/Gallery'
+// import Service from '../modules/service/container/Service'
+// import Gallery from '../modules/gallery/containers/Gallery'
 
 // import Navbar from '../shared/container/navbarDesktop'
-import MobileMenu from '../shared/container/navbarMobile'
+// import MobileMenu from '../shared/container/navbarMobile'
 
 const page = () => {
   return (
     <div className='text-3xl font-bold text-center'>
       {/* <Navbar/> */}
-    <MobileMenu ></MobileMenu>
+    {/* <MobileMenu ></MobileMenu> */}
+    <WelcomeHome />
+    <HeadLine />
+    <PhotoGallery />
       </div>
   )
 }
