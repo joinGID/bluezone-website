9e41fa555690c7290b1c4efe0aa88d87441661d5
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-12-25 20:29:47 +0530

    signature path change

--- src/modules/home/components/welcomehome/WelcomeText.jsx
+++ src/modules/home/components/welcomehome/WelcomeText.jsx
@@ -64,7 +64,7 @@ function WelcomeText() {
 
       {/* Signature */}
       <Image
-        src="/signature.png"
+        src="/welcomeImages/signature.png"
         alt="Signature"
         className="mx-auto"
         width={150}
